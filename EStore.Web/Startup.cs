b89b0ccedd266d47266715e337b60ddeb1cc3b79
Rustam Izmaylov <izmaylov-r@hotmail.com>
2021-07-26 12:30:23 +0300

    added swagger

--- EStore.Web/Startup.cs
+++ EStore.Web/Startup.cs
@@ -23,6 +23,7 @@ namespace EStore
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/dist"; });
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,7 +46,15 @@ namespace EStore
             {
                 app.UseSpaStaticFiles();
             }
+            app.UseSwagger();
 
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
