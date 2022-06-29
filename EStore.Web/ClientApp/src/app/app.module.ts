80aaf6a0481c927725ba2b6dd292942f1ac85520
Rustam Izmaylov <izmaylov-r@hotmail.com>
2022-06-29 11:02:22 +0300

    fix

--- EStore.Web/ClientApp/src/app/app.module.ts
+++ EStore.Web/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { CategoryComponent } from './category/category.component';
 import { ProductComponent } from './product/product.component';
 import {Category} from "./home/home.models";
@@ -20,8 +18,6 @@ import {FilterPipe} from "./pipes/filter.pipe";
         AppComponent,
         NavMenuComponent,
         HomeComponent,
-        CounterComponent,
-        FetchDataComponent,
         CategoryComponent,
         ProductComponent,
         FilterProductComponent,
@@ -33,9 +29,8 @@ import {FilterPipe} from "./pipes/filter.pipe";
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'category/:id', component: CategoryComponent },
+      { path: 'product/:id', component: ProductComponent }
     ])
   ],
   providers: [],
--- EStore.Web/ClientApp/src/app/home/home.service.ts
+++ EStore.Web/ClientApp/src/app/home/home.service.ts
@@ -9,19 +9,26 @@ import {Product} from "../product/product.models";
 })
 export class HomeService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   getAllCategories(): Observable<Category[]> {
     return this.http.get<Category[]>('category/getAllCategories');
   }
+
   getAllProductsByCategoryId(id: number): Observable<Product[]> {
     return this.http.get<Product[]>('category/getAllProductsByCategoryId?id=' + id);
   }
+
   getAllSpecificationByCategory(id: number): Observable<Specification[]> {
     return this.http.get<Specification[]>('category/getAllSpecificationByCategory?id=' + id);
   }
+
   getAllProductsSortByPrice(categoryId: number, minPrice: number, maxPrice: number, productOrder: number): Observable<Product[]> {
     return this.http.get<Product[]>('product/getAllProductsSortByPrice?' + `categoryId=${categoryId}&minPrice=${minPrice}&productOrder=${productOrder}&maxPrice=${maxPrice}`)
   }
-}
 
+  getProductById(id: number): Observable<Product[]> {
+    return this.http.get<Product[]>('product/getProductById?' + `id=${id}`)
+  }
+}
